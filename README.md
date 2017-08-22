8414a2d95342b79c2f86875a891ab3281fd2b1ea
George Zhang <georgejzhang@gmail.com>
2017-08-22 16:12:42 -0400

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# centroid_oracle_db
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: centroid_oracle_db
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
